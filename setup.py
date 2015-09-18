a3060d024bd22d83b5ee16425edf5c307d4e9515
Tomaz Berisa <tomaz.berisa@gmail.com>
2015-09-18 15:55:54 -0400

    Proper bitbucket link

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ if sys.version_info<(3,):
 setup(
   name = 'ldetect',
   packages = ['ldetect', 'ldetect.baselib', 'ldetect.examples', 'ldetect.pipeline_elements'],
-  version = '0.2.4',
+  version = '0.2.5',
   description = 'Package for detecting regions of linkage disequilibrium in the human genome',
   install_requires = ['numpy', 'scipy', 'matplotlib', 'commanderline'],
   author = 'Tomaz Berisa',
   author_email = 'tomaz.berisa@gmail.com',
-  url = 'https://bitbucket.org/tomazberisa/ldetect', 
-  download_url = 'https://bitbucket.org/tomazberisa/ldetect/get/0.2.4.tar.gz', 
+  url = 'https://bitbucket.org/nygcresearch/ldetect', 
+  download_url = 'https://bitbucket.org/nygcresearch/ldetect/get/0.2.5.tar.gz', 
   keywords = ['LD', 'linkage disequilibrium', 'genome', 'bioinformatics', 'ldetect'], 
   classifiers = [],
 )
